b683b63f8a57fb4134ab20dbb1af7d46d412eaff
Mpendulo <mpendu2016@gmail.com>
2025-04-13 22:15:04 +0200

    Mpendulo KIKUYU BUTTON ADDED

---
+++ TranslateButton.js
@@ -0,0 +1,69 @@
+
+const openBtn1 = document.getElementById("openModall1");
+const closeBtn1 = document.getElementById('closeModall1');
+const modall1 = document.getElementById("modall1");
+
+openBtn1.addEventListener("click", () => {
+    modall1.classList.add("open");
+});
+
+closeBtn1.addEventListener("click", () => {
+    modall1.classList.remove("open")
+})
+
+
+
+const openBtn2 = document.getElementById("openModall2");
+const closeBtn2 = document.getElementById('closeModall2');
+const modall2 = document.getElementById("modall2");
+
+openBtn2.addEventListener("click", () => {
+    modall2.classList.add("open");
+});
+
+closeBtn2.addEventListener("click", () => {
+    modall2.classList.remove("open")
+})
+
+const openBtn3 = document.getElementById("openModall3");
+const closeBtn3 = document.getElementById('closeModall3');
+const modall3 = document.getElementById("modall3");
+
+openBtn3.addEventListener("click", () => {
+    modall3.classList.add("open");
+});
+
+closeBtn3.addEventListener("click", () => {
+    modall3.classList.remove("open")
+})
+
+const openBtn4 = document.getElementById("openModall4");
+const closeBtn4 = document.getElementById('closeModall4');
+const modall4 = document.getElementById("modall4");
+
+openBtn4.addEventListener("click", () => {
+    modall4.classList.add("open");
+});
+
+closeBtn4.addEventListener("click", () => {
+    modall4.classList.remove("open")
+})
+
+const openBtn5 = document.getElementById("openModall5");
+const closeBtn5 = document.getElementById('closeModall5');
+const modall5 = document.getElementById("modall5");
+
+openBtn5.addEventListener("click", () => {
+    modall5.classList.add("open");
+});
+
+closeBtn5.addEventListener("click", () => {
+    modall5.classList.remove("open")
+})
+
+
+
+
+
+
+
--- celebpage.js
+++ celebpage.js
@@ -1,38 +1,3 @@
-// const openModalButtons = document.querySelectorAll('[data-modal-target]')
-// const closeModalButtons = document.querySelectorAll('[data-close-button]')
-// const overlay = document.getElementById('overlay')
-
-// openModalButtons.forEach(button => {
-//     button.addEventListener('click',() => {
-//         const modal = document.querySelector(button.dataset.modalTarget)
-//         openModal(modal)
-
-    
-//     })
-// } )
-
-// closeModalButtons.forEach(button => {
-//     button.addEventListener('click',() => {
-//         const modal = button.closest('.modal')
-//         closeModal(modal)
-
-    
-//     })
-// } )
-
-// function openModal(modal) {
-//     if (modal == null) return
-//     modal.classList.add('active')
-//     overlay.classList.add('active')
-// }
-
-// function closeModal(modal) {
-//     if (modal == null) return
-//     modal.classList.remove('active')
-//     overlay.classList.remove('active')
-
-
-// }
 
 
 const buttonCeleb1 = document.getElementById("btnCeleb1");
@@ -102,78 +67,6 @@ buttonCeleb5.addEventListener("click", (event) => {
         audiosCeleb5.style.display ="none";
     }
 });
-
-
-
-const openBtn1 = document.getElementById("openModall1");
-const closeBtn1 = document.getElementById('closeModall1');
-const modall1 = document.getElementById("modall1");
-
-openBtn1.addEventListener("click", () => {
-    modall1.classList.add("open");
-});
-
-closeBtn1.addEventListener("click", () => {
-    modall1.classList.remove("open")
-})
-
-
-
-const openBtn2 = document.getElementById("openModall2");
-const closeBtn2 = document.getElementById('closeModall2');
-const modall2 = document.getElementById("modall2");
-
-openBtn2.addEventListener("click", () => {
-    modall2.classList.add("open");
-});
-
-closeBtn2.addEventListener("click", () => {
-    modall2.classList.remove("open")
-})
-
-const openBtn3 = document.getElementById("openModall3");
-const closeBtn3 = document.getElementById('closeModall3');
-const modall3 = document.getElementById("modall3");
-
-openBtn3.addEventListener("click", () => {
-    modall3.classList.add("open");
-});
-
-closeBtn3.addEventListener("click", () => {
-    modall3.classList.remove("open")
-})
-
-const openBtn4 = document.getElementById("openModall4");
-const closeBtn4 = document.getElementById('closeModall4');
-const modall4 = document.getElementById("modall4");
-
-openBtn4.addEventListener("click", () => {
-    modall4.classList.add("open");
-});
-
-closeBtn4.addEventListener("click", () => {
-    modall4.classList.remove("open")
-})
-
-const openBtn5 = document.getElementById("openModall5");
-const closeBtn5 = document.getElementById('closeModall5');
-const modall5 = document.getElementById("modall5");
-
-openBtn5.addEventListener("click", () => {
-    modall5.classList.add("open");
-});
-
-closeBtn4.addEventListener("click", () => {
-    modall4.classList.remove("open")
-})
-
-
-
-
-
-
-
-
 
 
 const buttonCeleb6 = document.getElementById("btnCeleb6");
@@ -393,6 +286,4 @@ buttonCeleb21.addEventListener("click", (event) => {
     } else {
         audiosCeleb21.style.display ="none";
     }
-});
-
-
+}); 
